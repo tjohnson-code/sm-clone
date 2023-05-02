4ee258c31e6fc533dcea18a48138296f3de9b9c3
Tim Johnson <tjjohnson.86@gmail.com>
2023-05-02 01:02:30 -0500

    added like/comment/share to posts

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,14 @@
+import {
+  ChatBubbleBottomCenterTextIcon,
+  HandThumbUpIcon,
+  ShareIcon,
+} from '@heroicons/react/24/solid';
 import Image from 'next/image';
 
 function Post({ name, message, email, postImage, image, timestamp }) {
   return (
     <div className="flex flex-col">
-      <div className="p-5 bg-gray-300 mt-5 rounded-t-2xl shadow-sm">
+      <div className="p-5 bg-gray-700 text-slate-50 mt-5 rounded-t-2xl shadow-sm">
         <div className="flex items-center space-x-2">
           <img
             className="rounded-full"
@@ -31,6 +36,21 @@ function Post({ name, message, email, postImage, image, timestamp }) {
           />
         </div>
       )}
+      {/* Like/Comment/Share */}
+      <div className="flex justify-between items-center rounded-b-2xl bg-gray-300 shadow-md text-orange-500 border-t">
+        <div className="inputIcon rounded-none rounded-bl-2xl">
+          <HandThumbUpIcon className="h-4" />
+          <p className="text-xs sm:text-base">Like</p>
+        </div>
+        <div className="inputIcon rounded-none">
+          <ChatBubbleBottomCenterTextIcon className="h-4" />
+          <p className="text-xs sm:text-base">Comment</p>
+        </div>
+        <div className="inputIcon rounded-none rounded-br-2xl">
+          <ShareIcon className="h-4" />
+          <p className="text-xs sm:text-base">Share</p>
+        </div>
+      </div>
     </div>
   );
 }
